8737973814bf6d79f75055c2807316351f7aa76c
alawi ahmed <alawiahmedabdullah.masfar@uit.ac.ma>
2024-05-17 17:45:38 +0100

    design taux change tout les pays

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,81 +2,161 @@
   <v-app id="home">
     <NavBar />
     <v-container fluid>
-      <v-card-title class="card-title">Convertisseur de devises <br> Vérifier les taux de change en temps réel</v-card-title>
-      <v-row>
-
-      <v-col cols="12" md="12" class="box">
-        <v-card class="currency-card">
-          <v-card-text>
-            <!-- Currency Converter Form -->
-            <v-form class="currency-form" @submit.prevent="convertCurrency">
-              <table class="table-box">
-                <thead>
-                  <tr class="title-menu">
-                      <th colspan="2"><h4 class="title-text">Vous envoyez exactement</h4></th>
-                      <th colspan="2"><h4 class="title-text">Récipiendaires Gets</h4></th>
-                  </tr>
-                  <tr class="amount-menu">
-                    <th class="text-fields"><v-text-field v-model="amount" label="Montant à convertir" type="number"></v-text-field></th> 
-                    <th class="choix"><v-select v-model="fromCurrency" :items="currencies" label="De la devise"></v-select></th>
-                    <th class="text-fields"> <v-text-field v-model="amount" label="Montant à convertir" type="number"></v-text-field></th>
-                    <th class="choix"><v-select v-model="toCurrency" :items="currencies" label="Vers la devise"></v-select></th>
-                  </tr>
-                  <tr class="text-affiche">
-                    <th  colspan="2">
-                      <h4><v-icon color=#6C6BBD left class="mr-0">fas fa-eur</v-icon>    Frais de virement :  3.80 USD</h4>
-                    </th>
-                    <th colspan="2">
-                      <h4> <v-icon color=#6C6BBD left class="mr-0">fas fa-eur</v-icon>    Arrivée prévue : <span>laundi</span></h4>
-                    </th> 
-                  </tr>
+      
+      <v-row class="head">
+          <v-card-title class="card-title">Convertisseur de devises <br> Vérifier les taux de change en temps réel</v-card-title>
+        
+        <v-col cols="12" md="12"  class="box">
+          <v-card class="currency-card" >
+            <v-card-text>
+              <!-- Currency Converter Form -->
+              <v-form class="currency-form" @submit.prevent="convertCurrency">
+                <table class="table-box">
+                  <thead>
                     <tr>
-                      <th colspan="2"  class="text-affiche"><h4> <v-icon color=#6C6BBD left class="mr-0">fas fa-eur</v-icon>    Montant total à convertir : <span>2296.20  USD</span></h4></th>
+                        <th colspan="2"  ><h4 class="title-text">Vous envoyez exactement</h4></th>
+                        <th colspan="2"><h4 class="title-text">Récipiendaires Gets</h4></th>
                     </tr>
-                </thead>
-              </table>
-              <!-- <table class="table-box">
-                  <thead> 
-                    <tr class="title-menu">
-                      <th ><h4 class="title-text">Vous envoyez exactement</h4></th><th></th>
-                      <th><h4 class="title-text">Récipiendaires Gets</h4></th><th></th>
+                    <tr class="amount-menu">
+                      <th class="text-fields"><v-text-field v-model="amount" label="Montant à convertir" type="number"></v-text-field></th> 
+                      <th><v-select  v-model="fromCurrency" :items="currencies" label="De la devise"></v-select></th>
+                      <th class="text-fields"> <v-text-field v-model="amount" label="Montant à convertir" type="number"></v-text-field></th>
+                      <th><v-select  v-model="toCurrency" :items="currencies" label="Vers la devise"></v-select></th>
                     </tr>
-                    <tr class="amount-menu"> 
-                        <th class="text-fields">
-                            <v-text-field v-model="amount" label="Montant à convertir" type="number"></v-text-field>
-                        </th> 
-                        <th>
-                            <v-select v-model="fromCurrency" :items="currencies" label="De la devise"></v-select>
-                        </th>
-                        <th class="text-fields">
-                            <v-text-field v-model="amount" label="Montant à convertir" type="number"></v-text-field>
-                        </th>
-                        <th>
-                            <v-select v-model="toCurrency" :items="currencies" label="Vers la devise"></v-select>
-                        </th>
-                    </tr> 
                     <tr class="text-affiche">
-                      <th>
-                        <h4><v-icon color=#6C6BBD left class="mr-0">fas fa-eur</v-icon>    Frais de virement :  3.80 USD</h4> 
+                      <th  colspan="2">
+                        <h4><v-icon color=#6C6BBD left class="mr-0">fas fa-eur</v-icon>    Frais de virement : <b> 3.80 USD</b></h4>
                       </th>
-                      <th></th>
                       <th colspan="2">
-                        <h4> <v-icon color=#6C6BBD left class="mr-0">fas fa-eur</v-icon>    Arrivée prévue : <span>laundi</span></h4>
-                      </th>
+                        <h4> <v-icon color=#6C6BBD left class="mr-0">fas fa-eur</v-icon>    Arrivée prévue : <b>laundi</b></h4>
+                      </th> 
                     </tr>
-                    <tr>
-                      <th colspan="2" class="text-affiche">
-                        <h4> <v-icon color=#6C6BBD left class="mr-0">fas fa-eur</v-icon>    Montant total à convertir : <span>2296.20  USD</span></h4>
-                      </th>
-                    </tr>
+                      <tr class="text-affiche">
+                        <th colspan="4"  class="text-affiches"><h4> <v-icon color=#6C6BBD left class="mr-0">fas fa-eur</v-icon>    Montant total à convertir : <b>2296.20  USD</b></h4></th>
+                      </tr>
                   </thead>
-                </table> -->
-              <v-btn class="currency-btn" color=#0a2157 type="submit">Commancez</v-btn>
-            </v-form>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+                </table>
+                <!-- <table class="table-box">
+                    <thead> 
+                      <tr class="title-menu">
+                        <th ><h4 class="title-text">Vous envoyez exactement</h4></th><th></th>
+                        <th><h4 class="title-text">Récipiendaires Gets</h4></th><th></th>
+                      </tr>
+                      <tr class="amount-menu"> 
+                          <th class="text-fields">
+                              <v-text-field v-model="amount" label="Montant à convertir" type="number"></v-text-field>
+                          </th> 
+                          <th>
+                              <v-select v-model="fromCurrency" :items="currencies" label="De la devise"></v-select>
+                          </th>
+                          <th class="text-fields">
+                              <v-text-field v-model="amount" label="Montant à convertir" type="number"></v-text-field>
+                          </th>
+                          <th>
+                              <v-select v-model="toCurrency" :items="currencies" label="Vers la devise"></v-select>
+                          </th>
+                      </tr> 
+                      <tr class="text-affiche">
+                        <th>
+                          <h4><v-icon color=#6C6BBD left class="mr-0">fas fa-eur</v-icon>    Frais de virement :  3.80 USD</h4> 
+                        </th>
+                        <th></th>
+                        <th colspan="2">
+                          <h4> <v-icon color=#6C6BBD left class="mr-0">fas fa-eur</v-icon>    Arrivée prévue : <span>laundi</span></h4>
+                        </th>
+                      </tr>
+                      <tr>
+                        <th colspan="2" class="text-affiche">
+                          <h4> <v-icon color=#6C6BBD left class="mr-0">fas fa-eur</v-icon>    Montant total à convertir : <span>2296.20  USD</span></h4>
+                        </th>
+                      </tr>
+                    </thead>
+                  </table> -->
+                <v-btn class="currency-btn" color=#0a2157 type="submit">Commancez</v-btn>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      
+      </v-row>
+      <v-row class="taux">
+        <v-col class="taux-title">
+          <h2>Les taux de change aujourd'hui</h2>
+          <th><v-select v-model="toCurrency" :items="currencies" label="Vers la devise"></v-select></th>
+        </v-col>
+        <v-col cols="12" md="12" class="tchange">
+          <v-card >
+          </v-card>
+          <v-table class="table-change">
+            <thead>
+              <tr>
+                <th>image</th>
+                <th class="nom-pays" colspan="2">Dollar australien</th>
+                <th><h3> 1.5087 <br><b>USD / AUD</b></h3></th>
+                <th><h3> 1.5087 <br><b>USD / AUD</b></h3></th>
+                <th><h3> 0.40 % <br><b>USD / AUD</b></h3></th>
+              </tr>
+              <tr>
+                <th>image</th>
+                <th class="nom-pays" colspan="2">Dollar US</th>
+                <th><h3> 1.5087 <br><b>USD / AUD</b></h3></th>
+                <th><h3> 1.5087 <br><b>USD / AUD</b></h3></th>
+                <th><h3> 0.40 % <br><b>USD / AUD</b></h3></th>
+              </tr>
+              <tr>
+                <th>image</th>
+                <th class="nom-pays" colspan="2">Pound British </th>
+                <th><h3> 1.5087 <br><b>USD / AUD</b></h3></th>
+                <th><h3> 1.5087 <br><b>USD / AUD</b></h3></th>
+                <th><h3> 0.40 % <br><b>USD / AUD</b></h3></th>
+              </tr>
+              <tr>
+                <th>image</th>
+                <th class="nom-pays" colspan="2">Yuan chinois Renminbi</th>
+                <th><h3> 1.5087 <br><b>USD / AUD</b></h3></th>
+                <th><h3> 1.5087 <br><b>USD / AUD</b></h3></th>
+                <th><h3> 0.40 % <br><b>USD / AUD</b></h3></th>
+              </tr>
+              <tr>
+                <th>image</th>
+                <th class="nom-pays" colspan="2">Euro</th>
+                <th><h3> 1.5087 <br><b>USD / AUD</b></h3></th>
+                <th><h3> 1.5087 <br><b>USD / AUD</b></h3></th>
+                <th><h3> 0.40 % <br><b>USD / AUD</b></h3></th>
+              </tr>
+              <tr>
+                <th>image</th>
+                <th class="nom-pays" colspan="2">Riyal Saudi </th>
+                <th><h3> 1.5087 <br><b>USD / AUD</b></h3></th>
+                <th><h3> 1.5087 <br><b>USD / AUD</b></h3></th>
+                <th><h3> 0.40 % <br><b>USD / AUD</b></h3></th>
+              </tr>
+              <tr>
+                <th>image</th>
+                <th class="nom-pays" colspan="2">Dirham United Arab Emirates</th>
+                <th><h3> 1.5087 <br><b>USD / AUD</b></h3></th>
+                <th><h3> 1.5087 <br><b>USD / AUD</b></h3></th>
+                <th><h3> 0.40 % <br><b>USD / AUD</b></h3></th>
+              </tr>
+              <tr>
+                <th>image</th>
+                <th class="nom-pays" colspan="2">Dirham marocain</th>
+                <th><h3> 1.5087 <br><b>USD / AUD</b></h3></th>
+                <th><h3> 1.5087 <br><b>USD / AUD</b></h3></th>
+                <th><h3> 0.40 % <br><b>USD / AUD</b></h3></th>
+              </tr>
+            </thead>
+          </v-table>
+        </v-col>
+
+      </v-row>
+      <v-row class="devices">
+
+        <v-col cols="12" md="12" >
+          
+        </v-col>
+
+      </v-row>
       <!-- <div class="head">
 
           <v-card-header>
@@ -184,18 +264,13 @@ export default defineComponent({
 <style scoped>
   .v-container {
     padding: 1.8% 0;
-  }
-  .head {
-    position: relative;
-    text-align: center;
-    padding: 0.8%;
-    height: 100%;
-    width: 100%;
-    color: #E7ECF6
+    justify-items: auto;
   }
   .head{
     content: " ";
     position: absolute;
+    padding: 0.8%;
+    color: #E7ECF6;
     height: 100%;
     width: 100%;
     background: #E7ECF6;
@@ -205,7 +280,7 @@ export default defineComponent({
     content: " ";
     position: absolute;
     top: 100%;
-    height: 100%;
+    height: 120%;
     width: 100%;
     background: #021946;
     /* transform: skew(3deg ,3deg); */
@@ -213,8 +288,8 @@ export default defineComponent({
   .devices {
     content: " ";
     position: absolute;
-    top: 200%;
-    height: 100%;
+    top: 220%;
+    height: 120%;
     width: 100%;
     background: #021946;
     /* transform: skew(3deg ,3deg); */
@@ -223,49 +298,52 @@ export default defineComponent({
     content: " ";
     justify-items: center;
     position: absolute;
-    margin-top: 3.8%;
+    margin-top: 16.8%;
     margin-left: 10%;
-    height: 55%;
+    height: 50%;
     width: 80%;
     background: #0a2157;
     border-radius: 10px;
+    box-shadow: 0 2px 4px rgb(0, 18, 74);
   }
   .card-title {
   /* Style the card title */
+  position: absolute;
   padding-top: 4%;
+  width: 100%;
   color: #021946;
   font-weight: 500;
   font-size: 2.2rem;
   text-align: center;
   font-weight: bold;
 
+
   }
-  .box {
-  /* Style the entire card component */
-  position: absolute;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  }
-  
  .v-card-content {
   /* Style the card content area */
   padding: 16px;
  }
  .table-box{
   /* justify-items: stretch; */
+  justify-items: auto;
+  justify-content: center;
+  position: relative;
   padding-left: 5%;
-  padding-top: 2%;
+  padding-top: 1.2%;
   width: 98%;
  }
- .title-menu{
-    /* padding: 22px; */
- }
-
  .title-text {
     text-align: left;
+    font-size: 0.98rem;
  }
  .text-affiche{
     text-align: left;
-    padding-top: 2.2%;
+    padding-top: 3.8%;
+    font-size: 1.25rem;
+ }
+ .text-affiches{
+    text-align: left;
+    padding-top: 1.8%;
  }
  .amount-menu hr{
   /* display: ruby-base-container; 
@@ -273,23 +351,17 @@ export default defineComponent({
   justify-content: space-between;  */
   /* width: 100%; */
 
-}
-.choix{
-  /* width: 0%; */
-}
-.text-fields{
-  width: 30%;
-}
-  .v-text-field {
-    /* Style the amount text field */
-    /* width: 100%; */
-
   }
+
+  .text-fields{
+    width: 25%;
+  }
+
+
   .v-select {
     /* Style the currency selection dropdown */
-    /* padding-left: 0%;
-    padding: 0%; */
-    width: 180px;
+    /* width: 180px; */
+    width: 68%;
   }
   .currency-btn {
     /* Style the convert button */
@@ -299,13 +371,13 @@ export default defineComponent({
   }
 
   .currency-btn:hover {
-    background-color: #e4e7f4; /* Darker green on hover */
+    background-color: #f2f4e4; /* Darker green on hover */
   }
 
   .card-actions {
     /* Style the card actions section */
-    /* padding: 8px;
-    color: #E7ECF6; */
+    padding: 12%;
+    color: #E7ECF6;
   }
 
   .converted-amount {
@@ -314,17 +386,59 @@ export default defineComponent({
     font-weight: bold;
     font-size: 1.1rem;
   }
-
-  /************************************************************* */
   .currency-card {
   margin-bottom: 2%;
-}
-.currency-form {
+  box-shadow: 0 2px 4px rgb(255, 255, 255);
+  }
+  .currency-form {
+    margin-top: 2%;
+  }
+  .currency-btn {
+    margin-top: 5%;
+    margin-left: 3%;
+  }
+  .taux-title{
+    position: absolute;
+    display: flex;
+    justify-content: space-evenly;
+    top: 10%;
+    height: 6%;
+    left: 3.2%;
+    width: 50%;
+    padding: 0px;
+    background: #FFEAE3;
+    border-radius: 2px;
+    box-shadow: 0 2px 4px rgb(248, 249, 201);
+    text-align: right;
+  }
   
-  margin-top: 2%;
-}
-.currency-btn {
-  margin-top: 50px;
-}
+  /* .taux-title,v-select{
+    width: 60%;
+  } */
+  .tchange {
+    position: absolute;
+    top: 25%;
+    left: 3.2%;
+    height: 65;
+    width: 94%;
+    background: #FFEAE3;
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgb(248, 249, 201);
+  }
+  
+  .table-change ,h3{
+    position: relative;
+    text-align: center  ;
+  }
+  .nom-pays{
+    font-size: 135%;
+    font-style: unset;
+    font-weight: bold;
+    letter-spacing: 0.1em;
+  }
+  .nom-pays:hover{
+    background-color:#E7ECF6;
+    letter-spacing: 0.19em;
 
+  }
 </style>
